4adab19ef4ae6ab044fe680582762284bfcfd84a
Joseph Lee <jlee01@stuy.edu>
2019-03-21 07:20:29 -0400

    merge works! integrated insertion!

--- merge.java
+++ merge.java
@@ -7,25 +7,29 @@ public class merge{
         }
         System.out.println();
         test = new int[]{0,5,2,5,3,5,7,8,1};
-        insertionSort(test,1,6);
+        insertionSort(test,0,4);
         for (int i: test){
             System.out.print(i);
         }
     }
     /*sort the array from least to greatest value. This is a wrapper function*/
     public static void mergesort(int[]data){
-        int[] temp = new int[data.length];
-        for(int i = 0; i < data.length; i++){
-            temp[i] = data[i];
-        }
         mergesort(data,0,data.length-1);
     }
     private static void mergesort(int[]data, int lo, int hi){
         if (lo >= hi){
             return;
         }
-        mergesort(data,lo,hi/2);
-        mergesort(data,hi/2,hi);
+        System.out.println(lo + ", " + hi);
+        if(hi - lo < 43){
+            insertionSort(data,lo,(lo + hi+ 2)/2);
+            //System.out.print("crossed");
+            insertionSort(data,(lo + hi + 2)/2,hi + 1);
+        } else{
+            mergesort(data,lo,(lo + hi)/2);
+            //System.out.print("crossed");
+            mergesort(data,(lo + hi + 2)/2,hi);
+        }
         merge(data, lo, hi);
     }
     private static void merge(int[] data, int lo, int hi){
@@ -39,13 +43,25 @@ public class merge{
             temp2[i] = data[lo+temp1.length + i];
         }
         for(int i = lo, one = 0, two = 0; i <= hi; i++){
-            if (temp1[one]<temp2[two]){
-                data[i]=temp1[one];
-                one++;
+            if (one < temp1.length){
+                if (two < temp2.length && temp1[one]>temp2[two]){
+                    data[i]=temp2[two];
+                    two++;
+                }
+                else{
+                    data[i]=temp1[one];
+                    one++;
+                }
             }
             else {
-                data[i]=temp2[two];
-                two++;
+                if (one < temp1.length && temp1[one]<temp2[two]){
+                    data[i]=temp1[one];
+                    one++;
+                }
+                else {
+                    data[i]=temp2[two];
+                    two++;
+                }
             }
         }
     }
